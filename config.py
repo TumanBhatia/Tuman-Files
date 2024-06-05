a7df957463b27ea011a28ab3faf9f8d1994a600e
TumanBhatia <143202475+TumanBhatia@users.noreply.github.com>
2024-06-06 00:29:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ DB_NAME = environ.get("DB_NAME", "vjbotz")
 
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "7121414283:AAEiCnUwpMpQAucMMREe8_z9ldes-KHFEYs")
-BOT_USERNAME = environ.get("BOT_USERNAME", "@tumanfilessbot) # your bot username without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "tumanfilessbot) # your bot username without @
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/66604e5911df5a0c43ffa.jpg https://graph.org/file/f5a26ac21b060d5787ea3.jpg https://graph.org/file/30539d8f1fc92423d6a19.jpg https://graph.org/file/701ff72fb43d85e073f03.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
